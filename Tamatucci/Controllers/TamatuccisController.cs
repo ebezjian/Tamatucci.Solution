1e0fa1fe7528fd1e444beca7a1cfaf5cec52a14c
Ellie Lambert <ebezjian@gmail.com>
2020-12-16 18:20:56 -0500

    bug fix controllers and view folders
    Co-authored-by: Shannon Grantski <grantski@pm.me>

--- Tamatucci/Controllers/TamatuccisController.cs
+++ Tamatucci/Controllers/TamatuccisController.cs
@@ -4,13 +4,39 @@ using System.Collections.Generic;
 
 namespace TamaTucci.Controllers
 {
-  public class TuccisController : Controller
+  public class TamatuccisController : Controller
   {
     [HttpGet("/tamatuccis")]
     public ActionResult Index()
     {
       List<Tucci> allTuccis = Tucci.GetAll();
       return View(allTuccis);
+    }
+    [HttpGet("/tamatuccis/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/tamatuccis")]
+    public ActionResult Create(string tucciName)
+    {
+      Tucci myTucci = new Tucci(tucciName);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/tamatuccis/delete")]
+    public ActionResult Delete()
+    {
+      Tucci.ClearAll();
+      return View();
+    }
+
+    [HttpGet("/tamatuccis/{id}")]
+    public ActionResult Show(int id)
+    {
+      Tucci foundTucci = Tucci.Find(id);
+      return View(foundTucci);
     }
   }
 }
