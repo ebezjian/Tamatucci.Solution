63191a263f25db9d57283c2da56e5edc3be82f7a
Ellie Lambert <ebezjian@gmail.com>
2020-12-16 17:45:12 -0500

    add to views folders
    Co-authored-by: Shannon Grantski <grantski@pm.me>

--- Tamatucci/Models/Tamatucci.cs
+++ Tamatucci/Models/Tamatucci.cs
@@ -25,6 +25,11 @@ namespace TamaTucci.Models
       _instances.Add(this);
       Id = _instances.Count;
     }
+
+    public static List<Tucci> GetAll()
+    {
+      return _instances;
+    }
     public static void ClearAll()
     {
       _instances.Clear();
