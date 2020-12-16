c5851169f77fcd5e6d7f38568682bdee551fb118
Ellie Lambert <ebezjian@gmail.com>
2020-12-16 13:20:05 -0500

    add Tucci class and constructor
    Co-authored-by: Shannon Grantski <grantski@pm.me>

---
+++ Tamatucci/Models/Tamatucci.cs
@@ -0,0 +1,31 @@
+//Constructors and anything that modifies the object
+
+  //name submit form
+  // these will be defiend by ints:
+  // Feed, Play, Sleep  
+  //It will die
+  // ID number for all the tucci's
+  //buttons
+
+using System.Collections.Generic;
+
+namespace TamaTucci.Models
+{
+  public class Tucci
+
+  {
+    public string TucciName { get; set;}
+    public int Id { get; } //the program will handle this
+    private static TucciList<Tucci> _instances = new TucciList<Tucci> {}; //this will hold all of our Tucci's
+
+//Constructor that is building our Tucci
+    public Tucci(string tucciName)
+    {
+      TucciName = tucciName;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+  }
+
+  {}
+}
